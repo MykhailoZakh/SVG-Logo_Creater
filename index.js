d893c3d0be3a75f2c3c7b66e6e7ccf0224774c47
Mykhailo <sharkon90@gmail.com>
2023-12-18 17:02:05 -0500

    adding classes for shape, and adding fuction to print swg file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // initializing packages needed for the application
 const inquirer = require("inquirer");
 const fs = require("fs");
+const Circle = require("./lib/shapes")
 
 // function to initialize app
 const init = function(){
@@ -37,5 +38,9 @@ init();
 // function to create file with logo data
 
 function fileCreate(data) {
-    console.log(data);
+    let circleObj = new Circle(data.text, data.textColor, data.shapeColor);
+    // let dataText = 
+    let shape = circleObj.shape;
+    console.log(circleObj);
+    // fs.writeFile("./examples/logo.svg", circleObj.render(shape), (err) => err ? console.error(err) : console.log("Generated logo.svg"));
 }
